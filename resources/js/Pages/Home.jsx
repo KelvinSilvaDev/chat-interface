e797a667f84f1df1c417e4c8a1bda2603af73e66
Kelvin Oliveira <63024154+KelvinSilvaDev@users.noreply.github.com>
2023-10-23 10:39:49 -0300

    translate

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -5,8 +5,8 @@ export default function Home() {
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-100">
             <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-                <h1 className="text-2xl font-semibold mb-4">Bem-vindo ao Chat GPT</h1>
-                <p className="mb-8">Converse com nosso assistente de IA em tempo real.</p>
+                <h1 className="text-2xl font-semibold mb-4">Welcome to GPT chat</h1>
+                <p className="mb-8">Talk to our AI Chat in real time.</p>
                 <div className="space-x-4">
                     <InertiaLink
                         href={route('login')}
@@ -18,7 +18,7 @@ export default function Home() {
                         href={route('register')}
                         className="border border-blue-500 text-blue-500 py-2 px-4 rounded hover:bg-blue-100"
                     >
-                        Registrar
+                        Register
                     </InertiaLink>
                 </div>
             </div>
